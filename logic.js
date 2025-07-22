8717692f64d72e584b1d58cdff5392fc37542acf
rahman-cs-it <siddiquewasifur786123@gmail.com>
2025-07-22 06:50:16 +0530

    added BUtton EventLIstener

--- logic.js
+++ logic.js
@@ -1,3 +1,23 @@
-let guess = Math.floor(Math.random()*100)+1;
-console.log(guess);
+// console.log(guess);
+let outputBox = document.querySelector('.output')
 
+let button = document.querySelector('#Submit')
+button.addEventListener('click', () => {
+    // console.log(userinput.value);
+    let userinput = document.querySelector('#input');
+
+    let guess = Math.floor(Math.random() * 100) + 1;
+
+    checkLogic(userinput,guess);
+
+
+})
+
+
+function checkLogic(input , random){
+    
+    const p = document.createElement('p')
+    p.classList.add('dilogBox')
+
+    outputBox.appendChild(p)
+}
